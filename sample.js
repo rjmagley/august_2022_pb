6d208c9908e51ddb8954f8d8d555d3570b88bd43
Ryan Magley <ryan.magley@gmail.com>
2022-08-01 15:35:24 -0500

    08/01 commit

---
+++ sample.js
@@ -0,0 +1,24 @@
+var variableA = 7;
+var variableB = 3.14159;
+// whole numbers and floating points are both numbers
+// the symbol // indicates a comment
+
+
+// creating a variable - declare
+// give a variable a value - assignment
+
+var sampleString = 'Hello!';
+
+// console.log(variableA + '7');
+// string concatenation
+
+var newVariable = null;
+
+console.log(newVariable);
+// null explicitly means that there is no data present
+// undefined means that your statement just can't find appropriate data
+
+// booleans ('bool')
+// true or false
+
+var newBoolean = true;
